9b9817cb1c604f6e3770d2226a8e7ca6977ca2ed
sarosh14 <sarosh.coer@gmail.com>
2019-07-29 11:19:08 +0530

    buses model

--- JS/leafletwebapp/buses/models.py
+++ JS/leafletwebapp/buses/models.py
@@ -6,15 +6,18 @@ import django
 from django.contrib.gis.db import models as geomodels
 
 class Buses(models.Model):
+    
+    trip_id = models.IntegerField(blank=True)
+    route_id = models.IntegerField(blank=True)
+    # geometry = geomodels.PointField()
+    latitude = models.FloatField(blank=True,default=0.0)
+    longitude = models.FloatField(blank=True,default=0.0)
+    speed = models.FloatField(blank=True)
     vehicle_id = models.CharField(max_length=100, blank=False)
-    trip_id = models.IntegerField()
-    route_id = models.IntegerField()
-    geometry = geomodels.PointField()
-    speed = models.FloatField()
     timestamp = models.DateTimeField(default=django.utils.timezone.now)
 
     class Meta:
         # order of drop-down list items
-        ordering = ('vehicle_id','trip_id','route_id','geometry','speed','timestamp',)
+        ordering = ('vehicle_id','trip_id','route_id','latitude','longitude','speed','timestamp',)
         # plural form in admin view
         verbose_name_plural = 'buses'
