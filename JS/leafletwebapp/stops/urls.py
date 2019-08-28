099aa4835fb3e7f31bd89f7507f5d982b08e5787
Ashish Jain <ashishjain030495@live.com>
2019-08-28 17:49:45 +0530

    live update working multiple bus plot working

--- JS/leafletwebapp/stops/urls.py
+++ JS/leafletwebapp/stops/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
-from .views import AllStops,StopsDetailView,StopsTemplateView,AllBuses,BusesDetailView,particular_bus_id
+from .views import (AllStops,StopsDetailView,StopsTemplateView,AllBuses,BusesDetailView,
+particular_bus_id,particular_bus_id,add_bus_to_list,particular_buses_multiple)
 
 app_name = 'stops'
 
@@ -22,6 +23,12 @@ urlpatterns = [
 
         url(r'^allbuses/(?P<vehicle_id>[0-9|a-z|A-Z]+)$',
         particular_bus_id,name="particular-bus"),
+
+        url(r'^showbus/(?P<vehicle_id>[0-9|a-z|A-Z]+)$',
+        add_bus_to_list,name="add-bus-to-list"),
+        
+        url(r'^filtered_buses/$',
+        particular_buses_multiple,name="filtered-buses"),
 
         
 # url(r'^(?P<pk>[0-9]+)$', views.LocationsView, name='location-bus'),
--- JS/leafletwebapp/stops/views.py
+++ JS/leafletwebapp/stops/views.py
@@ -6,9 +6,18 @@ from django.shortcuts import render
 from django.core.serializers import serialize
 from django.http import HttpRequest, HttpResponse
 from stops.forms import StopsForm
+from django.utils import timezone
+from datetime import timedelta
 
 # class static_members():
 #     pass
+class FilterBuses:
+    time = 15
+    top_entries = 1
+    vehicle_id = []
+    filter_field = ""
+    route_id = []
+
 class StopsTemplateView(TemplateView):
     """
         Stops detail view.
@@ -34,8 +43,41 @@ class BusesDetailView(DetailView):
     template_name = 'stops-detail.html'
     model = Buses
 
+def print_filters():
+    print ("Routes Selected " + str(FilterBuses.route_id))
+    print ("Vehicles Selected " + str(FilterBuses.vehicle_id))
+    print ("Field Selected " + str(FilterBuses.filter_field))
+    print ("Time delta Selected " + str(FilterBuses.time))
+    print ("Top Selected " + str(FilterBuses.top_entries))
+
+def particular_buses_multiple(request):
+    print_filters()
+    if(len(FilterBuses.vehicle_id) > 0 and FilterBuses.filter_field == 'vehicle_id'):
+        filtered_buses = Buses.objects.filter(vehicle_id=FilterBuses.vehicle_id[0],
+        timestamp__gte=(timezone.now()-timedelta(minutes=FilterBuses.time))).order_by('-timestamp')[:FilterBuses.top_entries]
+        for i in range(1,len(FilterBuses.vehicle_id)):
+            filtered_buses = filtered_buses.union(Buses.objects.filter(vehicle_id=FilterBuses.vehicle_id[i],
+        timestamp__gte=(timezone.now()-timedelta(minutes=FilterBuses.time))).order_by('-timestamp')[:FilterBuses.top_entries])
+
+    elif(len(FilterBuses.route_id) > 0 and FilterBuses.filter_field == 'route_id'):
+        filtered_buses = Buses.objects.filter(vehicle_id__in=FilterBuses.vehicle_id,
+        timestamp__gte=(timezone.now()-timedelta(minutes=FilterBuses.time))).order_by('-timestamp')[:FilterBuses.top_entries]
+
+    else:
+        filtered_buses = Buses.objects.all().order_by('-timestamp')[:50]
+    buses_points = serialize('geojson',filtered_buses)
+    return HttpResponse(buses_points,content_type='json')
+
+def add_bus_to_list(request,vehicle_id):
+    FilterBuses.filter_field = "vehicle_id"
+    if(vehicle_id not in FilterBuses.vehicle_id):
+        FilterBuses.vehicle_id.append(vehicle_id)
+    FilterBuses.route_id = []
+    return particular_buses_multiple(request)
+
+
 def particular_bus_id(request,vehicle_id):
-    obj = Buses.objects.filter(vehicle_id=vehicle_id).order_by('-timestamp')[:1]
+    obj = Buses.objects.filter(vehicle_id=vehicle_id).order_by('-timestamp')[:FilterBuses.top_entries]
     # print (len(obj))
     # print (obj[0].speed)
     #print (vehicle_id+"hello here")
